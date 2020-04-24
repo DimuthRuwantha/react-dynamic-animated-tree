44b336d23bbbdbed6f1742dc3d46f8b64cd57d95
Dimuth Ruwantha <dimuth@CreativeSoftware.com>
2020-04-24 08:44:56 +0530

    Updates

---
+++ precache-manifest.d0d4879eee3346f3fa7104b88b92ca71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d78d6de0a5a1be52b938179a4ee07e",
+    "url": "/react-dynamic-animated-tree/index.html"
+  },
+  {
+    "revision": "f2a91e626ff7b4e7d969",
+    "url": "/react-dynamic-animated-tree/static/css/2.bfbf028b.chunk.css"
+  },
+  {
+    "revision": "e040507d597334f8e886",
+    "url": "/react-dynamic-animated-tree/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "f2a91e626ff7b4e7d969",
+    "url": "/react-dynamic-animated-tree/static/js/2.68d352d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-dynamic-animated-tree/static/js/2.68d352d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e040507d597334f8e886",
+    "url": "/react-dynamic-animated-tree/static/js/main.3d93e5d2.chunk.js"
+  },
+  {
+    "revision": "7d17eaf2b393a47e8d81",
+    "url": "/react-dynamic-animated-tree/static/js/runtime-main.23143b77.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-dynamic-animated-tree/precache-manifest.d0d4879eee3346f3fa7104b88b92ca71.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-dynamic-animated-tree/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
